3456133f30a04059d4b83d70847c99616c63a2ac
GeckoThePecko <bernie.gandin2@gmail.com>
2022-05-12 14:49:29 -0400

    geckolib: case-insensitive variable names in molang

--- src/main/java/com/eliotlash/molang/ast/Expr.java
+++ src/main/java/com/eliotlash/molang/ast/Expr.java
@@ -137,6 +137,21 @@ public interface Expr {
 		public <R> R accept(Visitor<R> visitor) {
 			return visitor.visitVariable(this);
 		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			Variable variable = (Variable) o;
+
+			return name != null ? name.equalsIgnoreCase(variable.name) : variable.name == null;
+		}
+
+		@Override
+		public int hashCode() {
+			return name != null ? name.toLowerCase().hashCode() : 0;
+		}
 	}
 
 	interface Visitor<R> {
--- src/main/java/com/eliotlash/molang/functions/FunctionDefinition.java
+++ src/main/java/com/eliotlash/molang/functions/FunctionDefinition.java
@@ -3,4 +3,5 @@ package com.eliotlash.molang.functions;
 import com.eliotlash.molang.ast.Expr;
 
 public record FunctionDefinition(Expr.Variable target, String member) {
+
 }
