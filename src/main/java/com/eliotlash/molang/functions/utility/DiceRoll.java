e2dd5648e193e2144911cb7742edf1d92a19deae
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-12-15 11:29:11 -0500

    Use `ThreadLocalRandom.current()` instead of keeping an instance of `Random` for each function Also add early exit for roll count `0`

--- src/main/java/com/eliotlash/molang/functions/utility/DiceRoll.java
+++ src/main/java/com/eliotlash/molang/functions/utility/DiceRoll.java
@@ -4,13 +4,12 @@ import com.eliotlash.molang.ast.Expr;
 import com.eliotlash.molang.functions.Function;
 import com.eliotlash.molang.variables.ExecutionContext;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class DiceRoll extends Function {
-	public java.util.Random random;
 
-
-	public DiceRoll(String name){
+	public DiceRoll(String name) {
 		super(name);
-		this.random = new java.util.Random();
 	}
 
 	@Override
@@ -21,11 +20,13 @@ public class DiceRoll extends Function {
 	public double _evaluate(Expr[] arguments, ExecutionContext ctx) {
 		double returnValue = 0;
 		double rollCount = this.evaluateArgument(arguments, ctx, 0);
-		double min = this.evaluateArgument(arguments, ctx, 1);
-		double max = this.evaluateArgument(arguments, ctx, 2);
+		if (rollCount > 0) {
+			double min = this.evaluateArgument(arguments, ctx, 1);
+			double max = this.evaluateArgument(arguments, ctx, 2);
 
-		for (int i = 0; i < rollCount; i++) {
-			returnValue += this.random.nextDouble() * (max - min) + min;
+			for (int i = 0; i < rollCount; i++) {
+				returnValue += ThreadLocalRandom.current().nextDouble() * (max - min) + min;
+			}
 		}
 
 		return returnValue;
